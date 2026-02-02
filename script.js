976b13bcd6f972d7d57d96392981445b3be8536d
luiz <lduducamposs@gmail.com>
2026-02-02 19:33:56 -0300

    função renderizarleads() pt1

--- script.js
+++ script.js
@@ -1,1 +1,70 @@
-let leads = []
+let leads = [
+    {
+        id: 1,
+        nome: "João Silva",
+        origem: "Instagram",
+        email: "joao@email.com",
+        telefone: "1199999999", 
+        data: "02/02/2026",
+        status: "oportunidade"
+
+    }
+]
+
+
+const listaCards = document.querySelector(".listaCards");
+console.log(listaCards);
+
+
+function renderizarLeads(){
+    const cardArticle = document.createElement("article")
+    cardArticle.classList.add("ArticleDoCard")
+
+
+    const divHeader = document.createElement("div")
+    cardHeader.classList.add("card-header")
+
+    const spanNome = document.createElement("span")
+    spanNome.classList.add("nomeLead")
+
+    const spanStatus = document.createElement("span")
+    spanStatus.classList.add("statusLead")
+
+
+
+
+
+    const divBody = document.createElement("div")
+    divBody.classList.add("cardBody")
+
+    const spanOrigem = document.createElement("span")
+    spanOrigem.classList.add("origemCard")
+
+    const spanEmail = document.createElement("span")
+    spanEmail.classList.add("emailCard")
+
+    const spanTelefone = document.createElement("span")
+    spanTelefone.classList.add("telefoneCard")
+
+    const spanData = document.createElement("span")
+    spanData.classList.add("dataCard")
+
+
+
+    const divCardAcoes = document.createElement("div")
+    divCardAcoes.classList.add("cardAcoes")
+
+    const btnApagar = document.createElement("button")
+    btnApagar.innerText = "🗑️";
+    btnApagar.classList.add("btn-apagar")
+
+    const btnEditar = document.createElement("button")
+    btnEditar.innerText = "🖊";
+    btnEditar.classList.add("btn-editar")
+
+    const btnAvancar = document.createElement("button")
+    btnAvancar.classList.add("btn-avancar")
+
+    
+
+}
